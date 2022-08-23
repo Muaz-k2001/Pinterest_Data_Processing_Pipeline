197077bb6e0fa6e8f4fe6344d232ae3ac24b8659
Muaz Khan <muaz.k2001@gmail.com>
2022-08-23 15:01:26 +0100

    Seperates consumers into batch and streaming groups.

--- batch_consumer.py
+++ batch_consumer.py
@@ -2,7 +2,7 @@ from kafka import KafkaConsumer
 import time
 
 
-consumer = KafkaConsumer('mytopic', group_id='KafkaGroup', bootstrap_servers=['localhost:9092'])
+consumer = KafkaConsumer('mytopic', group_id='batch', bootstrap_servers=['localhost:9092'])
 for message in consumer:
     message.value.decode('utf-8')
     print("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition, message.offset, message.key, message.value))

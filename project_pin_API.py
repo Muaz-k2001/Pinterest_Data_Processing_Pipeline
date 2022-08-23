b466ed623fce34b1ecd2c8238ee86f7aaa862445
Muaz Khan <muaz.k2001@gmail.com>
2022-08-23 13:45:47 +0100

    Edits POST method to send data to topic. Updates requirements.txt. Adds batch_consumer and streaming_consumer files

--- project_pin_API.py
+++ project_pin_API.py
@@ -25,6 +25,10 @@ class Data(BaseModel):
 @app.post("/pin/")
 def get_db_row(item: Data):
     data = dict(item)
+    json_data = dumps(data).encode('utf-8')
+    producer = KafkaProducer(bootstrap_servers='localhost:9092')
+    for _ in range(100):
+        producer.send('MyFirstKafkaTopic', json_data)
     return item
 
 
